405af9b617241fd136d5bb26fcc57395e8bf3265
Jerzy Sitek <j.sitek023@gmail.com>
2023-02-08 21:39:20 +0100

    Described Player moves

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// computer moves
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -13,3 +14,20 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// player moves
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+  playerMove = 'papier';
+} else if (playerInput == '3'){
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
